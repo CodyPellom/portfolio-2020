7937915a0a82c5de302cea8317007dbd184b01d1
Cody Pellom <cpellom@appliedsystems.com>
2019-12-29 09:50:35 -0500

    'initit'

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-var links = document.getElementById("navbarLink");
+var linkOne = document.getElementById("navbarLinkOne");
+var linkTwo = document.getElementById("navbarLinkTwo");
+var linkThree = document.getElementById("navbarLinkThree");
+var linkFour = document.getElementById("navbarLinkFour");
 
-    
-    function rainbow() {
-    this().classList.add("rainbow-text");
+
+function rainbow() {
+    linkOne.classList.add("rainbow-text");
+};
+
+function rainNo() {
+    linkOne.classList.remove("rainbow-text");
+};
+
+function rainbow2() {
+    linkTwo.classList.add("rainbow-text");
+};
+
+function rainNo2() {
+    linkTwo.classList.remove("rainbow-text");
+};
+
+
+function rainbow3() {
+    linkThree.classList.add("rainbow-text");
+};
+
+function rainNo3() {
+    linkThree.classList.remove("rainbow-text");
+};
+
+
+
+function rainbow4() {
+    linkFour.classList.add("rainbow-text");
+};
+
+function rainNo4() {
+    linkFour.classList.remove("rainbow-text");
 };
