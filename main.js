6ff6bf47c5bc0f25d3b0d0731e247f327f93dd6e
Cody Pellom <cpellom@appliedsystems.com>
2020-01-01 10:55:11 -0500

    'newYearsDayCommit'

--- main.js
+++ main.js
@@ -25,49 +25,7 @@ $(window).scroll(function () {
     $("#navbarSection").css("transition", "all 0.5s");
   }
 });
-var linkOne = document.getElementById("navbarLinkOne");
-var linkTwo = document.getElementById("navbarLinkTwo");
-var linkThree = document.getElementById("navbarLinkThree");
-var linkFour = document.getElementById("navbarLinkFour");
 
-
-
-
-
-function rainbow() {
-  linkOne.classList.add("rainbow-text");
-};
-
-function rainNo() {
-  linkOne.classList.remove("rainbow-text");
-};
-
-function rainbow2() {
-  linkTwo.classList.add("rainbow-text");
-};
-
-function rainNo2() {
-  linkTwo.classList.remove("rainbow-text");
-};
-
-
-function rainbow3() {
-  linkThree.classList.add("rainbow-text");
-};
-
-function rainNo3() {
-  linkThree.classList.remove("rainbow-text");
-};
-
-
-
-function rainbow4() {
-  linkFour.classList.add("rainbow-text");
-};
-
-function rainNo4() {
-  linkFour.classList.remove("rainbow-text");
-};
 
 var heroLogo = document.getElementById("heroLogo");
 var heroText = document.getElementById("heroText");
