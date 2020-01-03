6e7f50b60cf3a2535089978459d4ff66fe474131
Cody Pellom <cpellom@appliedsystems.com>
2020-01-03 09:24:21 -0500

    'animationProgressOnFriday'

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
- if(window.scrollY==0){
-    $("#navbarSection").css("opacity", "10");
-    $("#navbarSection").css("transition", "all 1s");
-    $('.hero-section').addClass("navbar-background-changer");
-    $('.overview-section').addClass("navbar-background-changer");
-    $('.hero-section').css("transition", "all 0.5s");
-    $('.overview-section').css("transition", "all 0.5s");
-  };
+var counter = 0;
+var counterTwo = 0;
+
+$.fn.isInViewport = function () {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+if (window.scrollY == 0) {
+  $("#navbarSection").css("opacity", "10");
+  $("#navbarSection").css("transition", "all 1s");
+  $('.hero-section').addClass("navbar-background-changer");
+  $('.overview-section').addClass("navbar-background-changer");
+  $('.hero-section').css("transition", "all 0.5s");
+  $('.overview-section').css("transition", "all 0.5s");
+};
 
 
 $(window).scroll(function () {
@@ -46,7 +57,7 @@ anime.timeline({
     easing: "easeOutExpo",
     duration: 2000,
     delay: (el, i) => 70 * (i + 1)
-  }) .add({
+  }).add({
     targets: '.ml5 .letters-left',
     opacity: [0, 1],
     translateX: ["0.5em", 0],
@@ -70,39 +81,58 @@ anime.timeline({
   })
 /*Hero Logo Animation End*/
 
- 
-/*Animation Section Start*/
-$.fn.isInViewport = function() {
-  var elementTop = $(this).offset().top;
-  var elementBottom = elementTop + $(this).outerHeight();
-  var viewportTop = $(window).scrollTop();
-  var viewportBottom = viewportTop + $(window).height();
-  return elementBottom > viewportTop && elementTop < viewportBottom;
-  };
-$(window).on('resize scroll', function() {
-$('.animation-section').each(function(){
-  if($('.animation-section').isInViewport()){
-    anime({
-      targets: '.animation-section .lines path',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: 'easeInOutSine',
-      duration: 2500,
-      delay: function(el, i) { return i * 250 },
-      direction: 'alternate',
-      loop: true
-    });
-  
-  
-  
-  
-  
-  
-  
 
- }
-})
+/*Animation Section Start*/
+
+$(window).on('resize scroll', function () {
+  $('.animation-trigger').each(function () {
+    if ($('.animation-trigger').isInViewport()) {
+      counter++;
+      if (counter <= 1) {
+        $('.animation-section').css("opacity", "1");
+        anime({
+          targets: '.animation-section .lines path',
+          strokeDashoffset: [anime.setDashoffset, 0],
+          easing: 'easeInOutSine',
+          duration: 10000,
+          delay: function (el, i) {
+            return i * 250
+          },
+          direction: 'alternate',
+          loop: false
+        });
+        $('#svgFill').css("transition", "fill 12.5s");
+        $('#svgFill').css("fill", "red");
+      };
+
+
+
+
+
+
+
+
+    }
+  })
 });
-    /*  anime({
+
+/*Companies Fade In Start*/
+$(window).on('resize scroll', function () {
+      $('.experience-section-companies').each(function () {
+            if ($('.experience-section-companies').isInViewport()) {
+              counterTwo++;
+              if (counter <= 1) {
+                
+                $('#appliedLogo').css("opacity", "1");
+                $('#hdLogo').css("opacity", "1");
+                $('#hpLogo').css("opacity", "1");
+                $('#trustfuelLogo').css("opacity", "1");
+              };
+            }}
+      )}
+)
+              /*Companies Fade In End*/
+              /*  anime({
       targets: '#triangleOne',
       translateX: 250, // -> '250px'
       rotate: 540,
